01dc1293db51ab49547637a10d78001a04b60b36
CrazyJaffar <84711098+CrazyJaffar@users.noreply.github.com>
2024-09-19 20:27:33 +0530

    Initial commit

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://continentaldatabase.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://continentaldatabase.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://continentaldatabase.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://continentaldatabase.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://continentaldatabase.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://continentaldatabase.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
